bdbe13ecacfe3927be7a93ed72dea55939a52dbe
neds <neds@example.com>
2025-09-15 13:35:52 +0200

    chore: update python flake

--- python-flake-template/flake.nix
+++ python-flake-template/flake.nix
@@ -1,48 +1,57 @@
 {
   description = "General Python project template with Nix flakes";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-  outputs = { self, nixpkgs }:
-    let
-      system = builtins.currentSystem;
-      pkgs = import nixpkgs { inherit system; };
+        # Select the Python version (edit for required Python version)
+        python = pkgs.python310; # Example: pkgs.python311 for Python 3.11
 
-      # Pick the Python version you want
-      python = pkgs.python311;
-
-      # Define your Python environment with deps
-      pythonEnv = python.withPackages (ps: with ps; [
-        requests
-        numpy
-        # flask
-        # fastapi
-        # jinja2
-        # add more here as needed
-      ]);
-    in {
-      # Development environment
-      devShells.default = pkgs.mkShell {
-        buildInputs = [ pythonEnv ];
-      };
-
-      # Package definition
-      packages.default = pkgs.buildPythonApplication {
-        pname = "my-python-app";
-        version = "0.1.0";
-        src = ./.;
-        propagatedBuildInputs = [
-          requests
-          numpy
+        # Select dependencies for both shell and app/package
+        pythonPackages = [
+          # pkgs.python310Packages.requests
+          # pkgs.python310Packages.numpy
+          # pkgs.python310Packages.flask
+          # pkgs.python310Packages.fastapi
+          # pkgs.python310Packages.jinja2
+          # Add more as needed
         ];
-      };
 
-      # Run your app via `nix run`
-      apps.default = {
-        type = "app";
-        program = "${pythonEnv}/bin/python ./main.py";
-      };
-    };
+        pythonEnv = python.withPackages (ps: pythonPackages);
+
+      in {
+        # Development shell: Editable Python code, dependencies available
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pythonEnv ];
+        };
+
+        # Package output: Makes 'nix build' work
+        packages.default = pkgs.python310Packages.buildPythonPackage {
+          pname = "my-python-app";
+          version = "0.1.0";
+          src = ./.;
+          format = "pyproject";
+          propagatedBuildInputs = pythonPackages;
+          doCheck = false; # Prevents test failures if package isn't tested
+        };
+
+        # App output: Makes 'nix run .#hello' work; add more as needed
+        apps = {
+          hello = {
+            type = "app";
+            program = "${pythonEnv.interpreter}/bin/python ./hello_world.py";
+          };
+          # Add more scripts/apps:
+          # another = {
+          #   type = "app";
+          #   program = "${pythonEnv.interpreter}/bin/python ./another_script.py";
+          # };
+        };
+      }
+    );
 }
