d5e0d6d0a60e279a7f11ca25ffac084a41b0791c
Michal Gorlas <m.gorlas@student.fontys.nl>
2025-09-15 21:03:14 +0200

    feat(go): initial template flake to be run in the container
    Signed-off-by: Michal Gorlas <m.gorlas@student.fontys.nl>

---
+++ golang-flake-template/template/default.nix
@@ -0,0 +1,22 @@
+{
+  pkgs ? (
+    let
+      inherit (builtins) fetchTree fromJSON readFile;
+      inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
+    in
+    import (fetchTree nixpkgs.locked) {
+      overlays = [
+        (import "${fetchTree gomod2nix.locked}/overlay.nix")
+      ];
+    }
+  ),
+  buildGoApplication ? pkgs.buildGoApplication,
+}:
+
+buildGoApplication {
+  pname = "u-root";
+  version = "v0.14.0";
+  pwd = ./.;
+  src = ./.;
+  modules = ./gomod2nix.toml;
+}
